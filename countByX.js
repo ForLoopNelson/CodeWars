117535bf8f91f78a043a963192ae0001178e5c7b
Cory Nelson <fullnelsontech@gmail.com>
2022-06-02 22:35:41 -0400

    counts up by x amount of times

---
+++ countByX.js
@@ -0,0 +1,12 @@
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+function countBy(x, n) {
+  const result = []
+  const limit = x * n
+
+  for (let i = 1; i <= limit; i++) {
+    if (i % x === 0) result.push(i)
+  }
+
+  return result
+}
